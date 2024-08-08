3eb6598d79051d4e44db08dc3a1e831d305740da
NJuGau <nathanael.gauthama@binus.ac.id>
2024-08-09 05:28:02 +0700

    Initialize project, make table view cell

---
+++ UIKitLearn/Views/CollectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionTableViewCell.swift
+//  UIKitLearn
+//
+//  Created by Nathanael Juan Gauthama on 09/08/24.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
