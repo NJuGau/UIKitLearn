68db3bc1af68f8cbe762ade5210684e721c243e7
NJuGau <nathanael.gauthama@binus.ac.id>
2024-08-09 04:26:34 +0700

    Initial Commit

---
+++ UIKitLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitLearn
+//
+//  Created by Nathanael Juan Gauthama on 09/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
