5c96cd4d8554f9f21317e8f19c766de827c4a693
NJuGau <nathanael.gauthama@binus.ac.id>
2024-08-12 00:36:03 +0700

    Make Header home view controller

---
+++ UIKitLearn/Managers/APICaller.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  UIKitLearn
+//
+//  Created by Nathanael Juan Gauthama on 12/08/24.
+//
+
+import Foundation
+
+
+class APICaller {
+    static let shared = APICaller()
+}
---
+++ UIKitLearn/Views/HeroHeaderUIView.swift
@@ -0,0 +1,85 @@
+//
+//  HeroHeaderUIView.swift
+//  UIKitLearn
+//
+//  Created by Nathanael Juan Gauthama on 11/08/24.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let downloadButton = {
+        let button = UIButton()
+         button.setTitle("Download", for: .normal)
+         button.layer.borderColor = UIColor.white.cgColor
+         button.layer.borderWidth = 1
+         button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 10
+         return button
+    }()
+    
+    private let playButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "HeroImage")
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        applyConstraint()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
+    private func applyConstraint() {
+        let playButtonConstraint = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 70),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        let downloadButtonConstraint = [
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -70),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraint)
+        NSLayoutConstraint.activate(downloadButtonConstraint)
+    }
+}
